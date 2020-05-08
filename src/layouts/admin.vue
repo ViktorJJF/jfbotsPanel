afab0298a6b11081fa506913120a6b8d35ca7af5
ViktorJJF <vj.jimenez96@gmail.com>
2020-05-08 13:35:30 -0500

    first commit

---
+++ src/layouts/admin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>aea alpaca</h1>
+    <sidebar></sidebar>
+  </div>
+</template>
+
+<script>
+import sidebar from "@/components/adminPanel/siderbar.vue";
+export default {
+  components: {
+    sidebar,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
